8ac09ce6d4d103c15be54d657a7f65a3dc86261c
Luzzer <rlarlgnsu@gmail.com>
2016-10-16 18:55:09 +0900

    Add files via upload

---
+++ CheckpointTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour {
+
+    public bool isTriggered;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Player") 
+        {
+            isTriggered = true;
+        }
+    }
+
+	
+}
